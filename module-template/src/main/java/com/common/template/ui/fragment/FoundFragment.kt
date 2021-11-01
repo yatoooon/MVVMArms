455f8ba0deab90efbe7ed587a16c324c17c3d2f7
yatoooon <qazasdeszplm@126.com>
2021-11-02 00:27:26 +0800

    foundfragment demo

--- module-template/src/main/java/com/common/template/ui/fragment/FoundFragment.kt
+++ module-template/src/main/java/com/common/template/ui/fragment/FoundFragment.kt
@@ -3,6 +3,9 @@ package com.common.template.ui.fragment
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.common.core.base.mvvm.BaseVMFragment
 import com.common.core.base.mvvm.BaseViewModel
+import com.common.res.App
+import com.common.res.ext.dp2px
+import com.common.res.ext.loadImage
 import com.common.res.immersionbar.BindImmersionBar
 import com.common.res.router.RouterHub
 import com.common.template.R
@@ -25,6 +28,15 @@ open class FoundFragment : BaseVMFragment<TemplateFragmentFoundBinding, BaseView
 
     override fun getImmersionBarType(): Int {
         return BindImmersionBar.DEFAULTBAR
+    }
+
+    override fun initView() {
+        loadImage(binding.ivFindCircle, R.drawable.res_example_bg, isCircle = true)
+        loadImage(
+            binding.ivFindCorner,
+            R.drawable.res_example_bg,
+            imageRadius = 200
+        )
     }
 
 }
