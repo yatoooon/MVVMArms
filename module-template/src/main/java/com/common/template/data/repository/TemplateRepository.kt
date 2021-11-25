f05da6f6bdee900666246d18e1edef3c3372eb1b
yatoooon <qazasdeszplm@126.com>
2021-11-25 23:35:23 +0800

    delete template datasource folder

--- module-template/src/main/java/com/common/template/data/repository/TemplateRepository.kt
+++ module-template/src/main/java/com/common/template/data/repository/TemplateRepository.kt
@@ -3,14 +3,13 @@ package com.common.template.data.repository
 import com.common.core.base.BaseRepository
 import com.common.res.net.apiCall
 import com.common.template.data.api.TemplateService
-import com.common.template.data.source.TemplatePagingSource
 import javax.inject.Inject
 
 
 class TemplateRepository @Inject constructor() : BaseRepository() {
 
-    suspend fun getArticleList(page: Int,pageSize:Int) = apiCall {
-        TemplatePagingSource(getRetrofitService(TemplateService::class.java)).getArticleList(page,pageSize)
+    suspend fun getArticleList(page: Int, pageSize: Int) = apiCall {
+        getRetrofitService(TemplateService::class.java).getArticleList(page, pageSize)
     }
 
 }
